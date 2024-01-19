2056f5584959975362b46e429d0798328df8ed02
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2024-01-19 13:51:00 -0500

    Update task_10.cpp

--- src/cpp/task_10.cpp
+++ src/cpp/task_10.cpp
@@ -4,6 +4,30 @@
  * Name:
  */
 
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
 class Problem10 {
 public:
+    static std::string generate3SATFormula(int numVariables, int numClauses) {
+        std::string formula;
+        std::srand(std::time(nullptr));
+
+        for (int i = 0; i < numClauses; ++i) {
+            std::string clause = "(";
+            for (int j = 0; j < 3; ++j) {
+                int var = std::rand() % numVariables + 1;
+                bool negation = std::rand() % 2;
+                clause += (negation ? "~" : "") + std::string(1, 'A' + var - 1);
+                if (j < 2) clause += " | ";
+            }
+            clause += ")";
+            if (i < numClauses - 1) clause += " & ";
+            formula += clause;
+        }
+
+        return formula;
+    }
 };
